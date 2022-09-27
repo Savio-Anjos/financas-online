f885a6aa507b0bc4b3e03178d2325fa649079356
unknown <Savioanjos2020@gmail.com>
2022-09-27 08:14:46 -0300

    Enviando arquivos

---
+++ javaScript/app.js
@@ -0,0 +1,27 @@
+  let janela
+
+  function abrirCriar() {
+  	window.location.href = (
+  		'http://localhost/financas_online/login.html') 	
+
+  } 
+
+   function abaCriarConta() {
+    window.location.href = (
+        'http://localhost/financas_online/criar_conta.html')
+  }
+
+  function cambioMoedas() {
+      window.location.href = (
+        'http://localhost/financas_online/cambio_moedas.html')
+
+  } 
+
+
+  
+
+   
+
+  
+
+ 
